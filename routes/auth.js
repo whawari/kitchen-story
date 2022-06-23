8160089d8c11541625359925baf9a5f793d6d812
Walid Hawari <walidhawari95@gmail.com>
2022-06-23 13:18:14 +0300

    [REFACTOR]: auth router

--- routes/auth.js
+++ routes/auth.js
@@ -3,23 +3,16 @@ const router = express.Router();
 const validateEmail = require("../middleware/validateEmail");
 const signup = require("../controllers/auth/signup");
 
-router.post(
-  "/signup",
-  validateEmail,
-  (req, res, next) => {
-    signup(req, res, next);
-  },
-  (req, res) => {
-    res.status(200).json({
-      message: "Account created successfully",
-      status: "success",
-    });
-  }
-);
+router.post("/signup", validateEmail, signup, (req, res) => {
+  res.status(200).json({
+    message: "Account created successfully",
+    status: "success",
+  });
+});
 
 router.use((error, req, res, next) => {
   if (error.statusCode && error.message) {
-    res.status(error.statusCode).json({
+    return res.status(error.statusCode).json({
       message: error.message,
       status: "fail",
     });
